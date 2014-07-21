aeb29c05cda1b1760bf33b782f8e6a592f49ca78
MaTachi <dannie92@gmail.com>
2014-07-21 20:29:47 +0200

    Add 404.php

---
+++ src/404.php
@@ -0,0 +1,19 @@
+<?php
+get_header(); ?>
+
+  <div class="col-sm-9" id="content">
+
+    <article id="post-0" class="post error404">
+      <header class="entry-header">
+        <h2 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'dunham-2036' ); ?></h1>
+      </header>
+
+      <div class="entry-content">
+        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'dunham-2036' ); ?></p>
+        <?php get_search_form(); ?>
+      </div><!-- .entry-content -->
+    </article><!-- #post-0 -->
+
+  </div><!-- #content -->
+
+<?php get_footer(); ?>
