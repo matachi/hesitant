80502ebcb2785c253af51a6ed40cd3cdc7065760
MaTachi <dannie92@gmail.com>
2014-07-23 12:38:40 +0200

    Add metadata to header

--- src/header.php
+++ src/header.php
@@ -1,12 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <link rel="icon" href="favicon.ico">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <!--[if lt IE 9]>
       <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
