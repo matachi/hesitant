0eff3cad4436fba45e56b3e4ad0d5e914d386252
Daniel Jonsson <daniel@jonsson.xyz>
2015-11-08 16:21:43 +0100

    Align footer better

--- src/footer.php
+++ src/footer.php
@@ -4,9 +4,13 @@
 
     <footer id="footer">
       <div class="container-fluid">
-        <p>© 2015 Lisa Jonsson, <a href="http://misslisibell.se">Misslisibell.se</a>.</p>
-        <p>Drivs med <a href="https://wordpress.org/">WordPress</a>.
-        <p><a href="http://misslisibell.se/om-cookies/">Om cookies</a> på Misslisibell.se.
+        <div class="row">
+          <div class="col-sm-10 col-sm-offset-1">
+            <p>© 2015 Lisa Jonsson, <a href="http://misslisibell.se">Misslisibell.se</a>.</p>
+            <p>Drivs med <a href="https://wordpress.org/">WordPress</a>.
+            <p><a href="http://misslisibell.se/om-cookies/">Om cookies</a> på Misslisibell.se.
+          </div>
+        </div>
       </div>
     </footer>
 
