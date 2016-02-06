4d96e10497b3c378cbeff92fafcb887a5bb5247f
Daniel Jonsson <daniel@jonsson.xyz>
2016-02-06 23:31:29 +0100

    Update year in footer

--- src/footer.php
+++ src/footer.php
@@ -6,7 +6,7 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-sm-10 col-sm-offset-1">
-            <p>© 2015 Lisa Jonsson, <a href="http://misslisibell.se">Misslisibell.se</a>.</p>
+            <p>© 2016 Lisa Jonsson, <a href="http://misslisibell.se">Misslisibell.se</a>.</p>
             <p>Drivs med <a href="https://wordpress.org/">WordPress</a>.
             <p><a href="http://misslisibell.se/om-cookies/">Om cookies</a> på Misslisibell.se.
           </div>
