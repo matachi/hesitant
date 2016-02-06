622d43c117d3194ac18d4bd25a4a9523965f8e51
Daniel Jonsson <daniel@jonsson.xyz>
2016-02-06 23:20:25 +0100

    Update how ads are displayed

--- src/content-ad.php
+++ src/content-ad.php
@@ -1,23 +1,33 @@
 <?php
 $client = 'client_code';
 $ads = array(
-  0 => 'slot_code',
-  1 => 'slot_code'
+  0 => array( 'id' => 'slot_code', 'format' => 'auto' ),
+  1 => array( 'id' => 'slot_code', 'format' => 'link' ),
+  2 => array( 'id' => 'slot_code', 'format' => 'link' ),
+  3 => array( 'id' => 'slot_code', 'format' => 'link' )
 );
 $id = get_query_var( 'hesitant_ad_id', 0 );
 
-if ($id > 0)
-  return
+if ( $id >= 0 && $id < sizeof( $ads ) ) {
 ?>
-<article>
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- Annons 3 -->
-  <ins class="adsbygoogle"
-    style="display:block"
-    data-ad-client="<?php print($client); ?>"
-    data-ad-slot="<?php print($ads[$id]); ?>"
-    data-ad-format="auto"></ins>
-  <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
-</article>
+  <article class="ad-slot <?php if ($id == 0) print( 'first' ); else print( 'following' ); ?> <?php if ($ads[$id]['format'] == 'auto') print( 'aut' ); else print( 'link' ); ?>">
+    <div>
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="<?php print($client); ?>"
+        data-ad-slot="<?php print($ads[$id]['id']); ?>"
+        data-ad-format="<?php print($ads[$id]['format']); ?>"></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </div>
+  </article>
+<?php
+}
+if ( $id == 0 ) {
+  function hesitant_adsense_script() {
+    print( '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' );
+  }
+  add_action( 'wp_footer', 'hesitant_adsense_script', 100 );
+}
+?>
